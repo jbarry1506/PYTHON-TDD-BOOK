cf80e2576b4b8f8ec47c30e2c7af2a31ba6e7f21
Jim Barry <jbarry1506@gmail.com>
2020-03-22 20:07:05 -0400

    changes

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,16 @@
+import selenium
 from selenium import webdriver
+import vars
+
 
 browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+browser.get(vars.my_localhost)
+
+try:
+    assert 'Django' in browser.title
+    print("Django success")
+except:
+    print("Django didn't work")
+
+browser.quit()
--- superlists/settings.py
+++ superlists/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import vars
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wqi!e2=5iu%ffji4%6m#0i*zopf#huq@qgyhso@sdd55#o-eyt'
+SECRET_KEY = vars.superlists_settings_secretkey
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
