1eed62803406f3b02592cc3fe1286c2656ce7ecc
Jim Barry <jbarry1506@gmail.com>
2020-03-25 21:34:17 -0400

    intentional test fail

--- functional_tests.py
+++ functional_tests.py
@@ -1,16 +1,39 @@
 import selenium
+import unittest
 from selenium import webdriver
 import vars
 
 
-browser = webdriver.Firefox()
+# ________________________________________________________________________
+# Chapter 1 Tutorial
+# browser = webdriver.Firefox()
 
-browser.get(vars.my_localhost)
+# browser.get(vars.my_localhost)
 
-try:
-    assert 'Django' in browser.title
-    print("Django success")
-except:
-    print("Django didn't work")
+# try:
+#     assert 'To-Do' in browser.title, "Browser title was " + browser.title
+#     print("Django success")
+# except:
+#     print("Django didn't work")
 
-browser.quit()
+# browser.quit()
+
+# ________________________________________________________________________
+# Chapter 2 tutorial
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    
+    def tearDown(self):
+        self.browser.quit()
+
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8080')
+        self.assertIn('To-Do', self.browser.title)
+        self.fail('Finish the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
