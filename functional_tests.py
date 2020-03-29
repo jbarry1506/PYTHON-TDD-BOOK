341857eb6ab77f2785b54ba15bc06fbb9bde6f94
Jim Barry <jbarry1506@gmail.com>
2020-03-29 15:53:43 -0400

    Added Functional test for list item additions

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,8 @@
 import selenium
-import unittest
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import unittest
+import time
 import vars
 
 
@@ -19,7 +21,7 @@ import vars
 # browser.quit()
 
 # ________________________________________________________________________
-# Chapter 2 tutorial
+
 class NewVisitorTest(unittest.TestCase):
 
     def setUp(self):
@@ -33,7 +35,28 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         self.browser.get('http://localhost:8080')
         self.assertIn('To-Do', self.browser.title)
+
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
+        
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
+
+        inputbox.send_keys('Buy peacock feathers')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows)
+        )
+
         self.fail('Finish the test!')
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
