f8d2f6ed6ef96d82b9b075fa533119d5df8ad34f
Jim Barry <jbarry1506@gmail.com>
2020-04-06 22:16:45 -0400

    removed voodoo wait, added try / except with MAX_WAIT

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,9 +2,11 @@ from django.test import LiveServerTestCase
 import selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
 import vars
 
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -15,6 +17,18 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
@@ -39,20 +53,19 @@ class NewVisitorTest(LiveServerTestCase):
 
         # user enters text for to-do list item
         inputbox.send_keys('Buy peacock feathers')
+        # When she hits enter, the page updates, and now the page lists
+        # "1: Buy peacock feathers" as an item in a to-do list table
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-        # test the input
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-
-        # user enters second item
+        # There is still a text box inviting her to add another item.
+        # She enters "Use peacock feathers to make a fly"
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(2)
 
-        # test the input
-        self.check_for_row_in_list_table('1: Buy peacock feathers')
-        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        # The page updates again, and now shows both items on her list
+        self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         self.fail('Finish the test!')
